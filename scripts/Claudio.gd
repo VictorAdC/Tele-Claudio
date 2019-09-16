ca867fd612772f2f3a2b21435c2fcebe098efcd4
Victor Costa <victor.adc@hotmail.com>
2019-09-16 13:51:01 -0300

    Claudio walking and jumping

--- scripts/Claudio.gd
+++ scripts/Claudio.gd
@@ -1,12 +1,17 @@
 extends KinematicBody2D
 
-const SPEED = 750
+const SPEED = 400
+const JUMP_FORCE = 1000
+const GRAVITY = 50
+const MAX_FALL_SPEED = 1000
+
+var motion = Vector2(0,0)
 
 func _physics_process(delta):
-	
-	var motion = Vector2(0,0)
+
 	var animation = get_node("AnimatedSprite")
-	
+
+	motion.x = 0
 	if Input.is_action_pressed("ui_right") && !Input.is_action_pressed("ui_left"):
 		motion.x += SPEED
 		animation.play("run")
@@ -18,4 +23,16 @@ func _physics_process(delta):
 	else:
 		animation.play("idle")
 
-	move_and_slide(motion)
+	var grounded = is_on_floor()
+
+	motion.y += GRAVITY
+	if grounded and Input.is_action_pressed("ui_up"):
+		motion.y = -JUMP_FORCE
+	if grounded and motion.y >= 0:
+		motion.y = 5
+	if motion.y > MAX_FALL_SPEED:
+		motion.y = MAX_FALL_SPEED
+	if !grounded:
+		animation.play("jump")
+
+	move_and_slide(motion, Vector2(0,-1))
